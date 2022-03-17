159549c8723f90942170e4a533b5ace52561361d
Ischi94 <gmathes94@gmx.de>
2022-03-17 07:50:22 +0100

    read in raw data and clean

---
+++ R/clean_data.R
@@ -0,0 +1,8 @@
+library(here)
+library(tidyverse)
+
+
+# load data ---------------------------------------------------------------
+
+dat_raw <- read_rds(here("data", 
+                 "raw_foraminifera_data.rds"))
