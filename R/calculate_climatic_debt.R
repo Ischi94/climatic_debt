7ce8fd233f83f2eb6b4ddba49e7e01bbbe16cafe
Ischi94 <gmathes94@gmx.de>
2022-03-21 12:54:51 +0100

    perform WAPLS on train data

--- R/calculate_climatic_debt.R
+++ R/calculate_climatic_debt.R
@@ -1,20 +1,77 @@
-library(raster)
 library(here)
 library(tidyverse)
-
+library(rioja)
 
 
 # load data ---------------------------------------------------------------
 
 # cleaned foraminifera occurrence time series along with temperature
 dat_spp <- read_rds(here("data", 
-                        "spp-and-sampling-data_list-by-depth.rds"))
+                        "spp_by_depth.rds")) %>% 
+  # right format for WAPLS
+  mutate(rel_abund = rel_abund * 100)
+
+# average global temperature in each bin
+dat_mean_temp <- read_csv(here("data", 
+                               "global-MAT_10-deg-grid_8ka.csv")) %>% 
+  select(bin, temp_ym_0m) 
+
+
+
+# estimate species temperature optima -------------------------------------
+
+
+# select training data for WA-PLS
+# select bins with average temperature based on quantiles
+dat_temp_quant <- dat_mean_temp %>% 
+  summarize(temp_quant = quantile(temp_ym_0m)) %>% 
+  pull(temp_quant) 
+
+dat_bins <- dat_mean_temp %>% 
+  filter(between(temp_ym_0m, dat_temp_quant[2], dat_temp_quant[4]))
+
+dat_train <- dat_spp %>% 
+  filter(bin %in% unique(dat_bins$bin))
+
+
+# get the species data in the right format
+dat_train_spec <- dat_train %>% 
+  mutate(rel_abund = rel_abund * 100) %>% 
+  pivot_wider(id_cols = c(core_uniq, bin), 
+              names_from = species, 
+              values_from = rel_abund, 
+              values_fn = mean, 
+              values_fill = 0) %>% 
+  # there is one row with na's
+  filter(!(core_uniq == "124_767B" & bin == 292)) %>% 
+  arrange(bin, core_uniq) %>% 
+  select(-c(core_uniq, bin)) %>% 
+  as.data.frame() 
+  
+
+# same with temperature data
+dat_train_temp <- dat_train %>% 
+  distinct(core_uniq, bin, temp_surface, temp_depth) %>% 
+  filter(!(core_uniq == "124_767B" & bin == 292)) %>% 
+  arrange(bin, core_uniq) %>% 
+  pull(temp_surface) 
+
+
+# fit the weighted average partial least squares model (WAPLS)
+mod_wapls <- WAPLS(dat_train_spec, dat_train_temp)
+
+# cross-validate model
+mod_cv <- crossval(mod_wapls, cv.method = "loo")
+
+# How many components to use based on RMSE via loo
+nr_comp <- performance(mod_cv)$crossval[, 1] %>% 
+  which.min(.)
 
 
 
 
 
-# species temperature index -------------------------------------
+
 
 # using surface water temperature
 dat_spp_surf <- dat_spp %>% 
