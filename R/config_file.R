a0001efd915f6730568301c171cc1c89203111d8
Ischi94 <gmathes94@gmx.de>
2022-03-23 09:01:27 +0100

    add config file for plotting

---
+++ R/config_file.R
@@ -0,0 +1,27 @@
+# general configurations
+
+#set ggplot output
+ggplot2::theme_set(ggplot2::theme_minimal(base_size = 10))
+
+ggplot2::theme_update(text = element_text(colour = "grey20", size = 10), 
+                      legend.text = element_text(colour = "grey20", size = 10),
+                      legend.title = element_text(colour = "grey20", size = 10),
+                      axis.text = element_text(colour = "grey20", size = 10), 
+                      strip.text = element_text(colour = "grey20", size = 10),
+                      panel.grid.minor = element_line(size = 0.1), 
+                      panel.grid.major = element_line(size = 0.3), 
+                      plot.title = element_text(colour = "grey20", size = 10))
+
+# define output sizes
+image_width <- 183
+image_height <- 100
+image_units <- "mm"
+
+# define pallets
+
+# define common color
+colour_lavender = "#9678B6"
+colour_brown = "#9C7673"
+colour_green = "#105157"
+colour_grey = "#D3CFC2"
+colour_coral = "coral2"
