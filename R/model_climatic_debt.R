f14a71841c321cde2e785c94ade725e22d5218f1
Ischi94 <gmathes94@gmx.de>
2022-04-04 11:59:27 +0200

    rename fourth plot

--- R/model_climatic_debt.R
+++ R/model_climatic_debt.R
@@ -246,6 +246,6 @@ plot_final <- plot_debt_time + plot_temp +  plot_mod_comp +
 
 # save plot_final
 ggsave(plot_final, filename = here("figures", 
-                                   "fig3_comparison.png"), 
+                                   "fig4_comparison.png"), 
        width = image_width, height = image_height, units = image_units, 
        bg = "white", device = ragg::agg_png)
